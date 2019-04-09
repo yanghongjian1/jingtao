1e0a4fff653e0708308875ded1852d2fcb32a136
SC-201901151420\Administrator <18722060630@163.com>
2019-04-09 17:00:24 +0800

    多余删除

--- jt-webpro/src/main/java/com/jingtao/jtorder/dubbo/api/dao/OrderMapper.java
+++
@@ -1,7 +0,0 @@
-package com.jingtao.jtorder.dubbo.api.dao;
-
-import com.jingtao.jtorder.dubbo.api.Mapper;
-import com.jingtao.jtorder.dubbo.api.model.Order;
-
-public interface OrderMapper extends Mapper<Order> {
-}
--- jt-webpro/src/main/java/com/jingtao/jtorder/dubbo/api/dao/OrderShippingMapper.java
+++
@@ -1,7 +0,0 @@
-package com.jingtao.jtorder.dubbo.api.dao;
-
-import com.jingtao.jtorder.dubbo.api.Mapper;
-import com.jingtao.jtorder.dubbo.api.model.OrderShipping;
-
-public interface OrderShippingMapper extends Mapper<OrderShipping> {
-}
